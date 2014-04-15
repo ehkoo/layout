1bcfeb0b5150d65ab6cfb46afaf1ad47347d1a25
azerozvn <nguoichet001@yahoo.com>
2014-04-15 20:04:43 +0700

    Responsive fixed, reduce padding size a bit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,6 @@
 $(function() {
     $('header').css('min-height', $(window).height());
-    $('.scroll.down')
-        .transition('set looping')
-        .transition('bounce', '2500ms');
-
-    var navbar = $('.ui.sidebar.navbar')
-    .sidebar({overlay: true})
-    .sidebar('attach events', '.menu.item');
+    $('.right.menu.open').on("click",function(e){
+		$('.ui.vertical.menu').toggle();
+	});
 });
